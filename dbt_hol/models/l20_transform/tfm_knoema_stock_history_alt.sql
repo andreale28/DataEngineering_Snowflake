80c38fcb694f0919c4bac54581a419aa7f1a9b8c
andreale28 <lhson43680@gmail.com>
2022-09-30 13:09:44 +0700

    2nd day of snowflake dbt tutorial

--- dbt_hol/models/l20_transform/tfm_knoema_stock_history_alt.sql
+++ dbt_hol/models/l20_transform/tfm_knoema_stock_history_alt.sql
@@ -1,5 +1,5 @@
 SELECT
-  company_ticker, company_name, stock_exchange_name, date_, data_source_name,
+  company_ticker, company_name, stock_exchange_name, date, data_source_name,
   {{ dbt_utils.pivot(
       column = 'indicator_name',
       values = dbt_utils.get_column_values(ref('base_knoema_stock_history'), 'indicator_name'),
